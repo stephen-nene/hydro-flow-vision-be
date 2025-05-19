b3c00fb79e696760c95e57d45ca25c909a2f74d6
devnene <stevekid705@gmail.com>
2025-05-19 14:12:52 +0300

    new ignore file

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt && \
-python manage.py makemigrations --verbosity 2 && \
+# python manage.py makemigrations --verbosity 2 && \
 python manage.py collectstatic && \
-# python manage.py migrate && \
-python manage.py migrate profiles && \
-python manage.py migrate management && \
+python manage.py migrate && \
+# python manage.py migrate profiles && \
+# python manage.py migrate management && \
 python manage.py seed_all

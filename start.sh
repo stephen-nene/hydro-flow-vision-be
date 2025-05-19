973ef598ac587a03dbfd5bcf2e228e299b0b59a8
devnene <stevekid705@gmail.com>
2025-05-19 13:58:02 +0300

    added requirements

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# uv pip install -r requirements.txt && \
+pip install -r requirements.txt && \
 python manage.py makemigrations --verbosity 2 && \
 python manage.py collectstatic && \
 python manage.py migrate && \

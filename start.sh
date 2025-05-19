f78f2982888a80a4d67e1fc716151ebe793a258f
devnene <stevekid705@gmail.com>
2025-05-19 13:59:45 +0300

    new migrate

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 pip install -r requirements.txt && \
 python manage.py makemigrations --verbosity 2 && \
 python manage.py collectstatic && \
-python manage.py migrate && \
-# python manage.py migrate profiles && \
-# python manage.py migrate management && \
+# python manage.py migrate && \
+python manage.py migrate profiles && \
+python manage.py migrate management && \
 python manage.py seed_all

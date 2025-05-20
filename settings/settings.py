234b354a25115e871acafeb119454e10848bb26d
devnene <stevekid705@gmail.com>
2025-05-20 09:43:17 +0300

    new stuff in rest

--- settings/settings.py
+++ settings/settings.py
@@ -93,13 +93,20 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# REST_FRAMEWORK = {
+#        'DEFAULT_AUTHENTICATION_CLASSES': (
+#            'rest_framework_simplejwt.authentication.JWTAuthentication',
+#        ),
+#    }
+
+
 REST_FRAMEWORK = {
        'DEFAULT_AUTHENTICATION_CLASSES': (
            'rest_framework_simplejwt.authentication.JWTAuthentication',
        ),
-       'DEFAULT_PERMISSION_CLASSES': (
-           'rest_framework.permissions.IsAuthenticated',
-       ),
+    #    'DEFAULT_PERMISSION_CLASSES': (
+    #        'rest_framework.permissions.IsAuthenticated',
+    #    ),
        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
        'PAGE_SIZE': 10,
        'DEFAULT_RENDERER_CLASSES': (
